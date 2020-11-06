9b9c1e36a24ec08f77cd659cb91f36c12a542a15
Nimeshchandhra1 <68285734+Nimeshchandhra1@users.noreply.github.com>
2020-11-06 19:41:41 +0530

    Fix

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.INFO)
 
 
 def start_cmd(update, context):
-    update.message.reply_text(f"Hello **{update.message.from_user.first_name}**.\n\nI can upload photos from telegram to telegra.ph 🤫                           `Bot By Nimesh chandhra`")
+    update.message.reply_text(f"Hello ***{update.message.from_user.first_name}***.\n\nI can upload photos from telegram to telegra.ph 🤫\n\n``Bot By Nimesh chandhra``")
 
 
 def upload_cmd(update, context):
@@ -25,7 +25,7 @@ def upload_cmd(update, context):
         update.message.reply_text(f"Failed to upload. Reason: {err}")
         return
     url = "https://telegra.ph" + info[0].get("src")
-    update.message.reply_text(url)
+    update.message.reply_text(url)\n\n Bot By NC
     os.remove(f'{str(update.message.from_user.id)}.jpg')
 
 
